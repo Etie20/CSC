a7ffd51b1de7354ce754ea5c42db886b0872f73e
Etie20 <114919553+Etie20@users.noreply.github.com>
2023-02-17 16:33:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+window.addEventListener("scroll", function (){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
